ec051df1452a919c418386f38b52ff6cd9257954
Dotan Simha <dotansimha@gmail.com>
2016-10-31 12:57:21 +0200

    feat(jsdoc): Minor fixes for docs generation scripts

--- generate-docs.sh
+++ generate-docs.sh
@@ -1,5 +1,6 @@
-mkdir temp
-tsc --outDir ./temp/ --target es6
+rm -rf temp &> /dev/null
+mkdir temp &> /dev/null
+tsc --outDir ./temp/ --target es6 &> /dev/null
 $(npm bin)/jsdoc2md -l js --no-cache --source "$(cat ./temp/MeteorObservable.js)" > ./docs/MeteorObservable.md
 $(npm bin)/jsdoc2md -l js --no-cache --source "$(cat ./temp/ObservableCollection.js)" > ./docs/ObservableCollection.md
 $(npm bin)/jsdoc2md -l js --no-cache --source "$(cat ./temp/ObservableCursor.js)" > ./docs/ObservableCursor.md
